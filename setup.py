fbbe6bc898c23ac012a7b4ff272f30525e64da5f
Songyuan Cui <cui20@illinois.edu>
2025-05-06 15:12:32 -0500

    Inital commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import numpy
+import pybind11
+from setuptools import Extension, setup
+
+
+def create_extension(name: str, sources: list[str]) -> None:
+    return Extension(
+        name,
+        sources=sources,
+        include_dirs=[pybind11.get_include(), numpy.get_include()],
+        language="c++",
+        extra_compile_args=["-O3", "-march=native", "-std=c++11"],
+    )
+
+
+ext_modules = [
+    create_extension(
+        "pydistmesh._triangle_utils", ["pydistmesh/src/triangle_utils.cpp"]
+    ),
+    create_extension(
+        "pydistmesh._tetrahedron_utils", ["pydistmesh/src/tetrahedron_utils.cpp"]
+    ),
+    create_extension("pydistmesh._internal", ["pydistmesh/src/internal.cpp"]),
+]
+
+setup(ext_modules=ext_modules)
