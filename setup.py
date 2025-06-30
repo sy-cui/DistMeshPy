572ea64b8ab569255984a701115b1d25444bb565
Songyuan Cui <cui20@illinois.edu>
2025-06-30 12:36:52 -0500

    fix: fixed the source file directories

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def create_extension(name: str, sources: list[str]) -> None:
     return Extension(
         name,
         sources=sources,
-        include_dirs=["distmeshpy/src", pybind11.get_include(), numpy.get_include()],
+        include_dirs=["distmeshpy/core/src", pybind11.get_include(), numpy.get_include()],
         language="c++",
         extra_compile_args=["-O3", "-march=native", "-std=c++11"],
     )
@@ -15,12 +15,12 @@ def create_extension(name: str, sources: list[str]) -> None:
 
 ext_modules = [
     create_extension(
-        "distmeshpy._triangle_utils", ["distmeshpy/src/triangle_utils.cpp"]
+        "distmeshpy.core._triangle_utils", ["distmeshpy/core/src/triangle_utils.cpp"]
     ),
     create_extension(
-        "distmeshpy._tetrahedron_utils", ["distmeshpy/src/tetrahedron_utils.cpp"]
+        "distmeshpy.core._tetrahedron_utils", ["distmeshpy/core/src/tetrahedron_utils.cpp", "distmeshpy/core/src/triangle_utils.cpp"]
     ),
-    create_extension("distmeshpy._internal", ["distmeshpy/src/internal.cpp"]),
+    create_extension("distmeshpy.core._internal", ["distmeshpy/core/src/internal.cpp"]),
 ]
 
 setup(ext_modules=ext_modules)
